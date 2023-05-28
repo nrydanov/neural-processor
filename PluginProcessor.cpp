9e5e2051f7478775e99953f396ddafc5e90cc094
Nikita Rydanov <56316351+nrydanov@users.noreply.github.com>
2023-05-28 23:07:45 +0400

    Added RTNeural support (#2)
    * Base RTNeural configuration

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -1,21 +1,25 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
-
+#include "RTNeural/RTNeural/RTNeural.h"
 //==============================================================================
 NeuralProcessor::NeuralProcessor()
      : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+    #if ! JucePlugin_IsMidiEffect
+        #if ! JucePlugin_IsSynth
+            .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+        #endif
+            .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+        #endif
+        ),
+    parameters(*this, nullptr, juce::Identifier("Parameters"), {
+        std::make_unique<juce::AudioParameterChoice>("modelType", "Model type", 
+            juce::StringArray {"LightModel", "MediumModel", "HeavyModel"}, 0)})
 {
-}
 
+}
 NeuralProcessor::~NeuralProcessor()
 {
+
 }
 
 //==============================================================================
@@ -125,43 +129,27 @@ template<typename T>
 void NeuralProcessor::processAbstractBlock (juce::AudioBuffer<T>& buffer,
                                                 juce::MidiBuffer& midiMessages)
 {
-    juce::ignoreUnused (midiMessages);
+    
+    juce::ignoreUnused(midiMessages);
 
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    using namespace juce;
+    ScopedNoDenormals noDenormals;
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
+    for (int channel = 0; channel < buffer.getNumChannels(); ++channel) {
+        T* outBuffer = buffer.getWritePointer(channel);
+        auto numSamples = buffer.getNumSamples();
+        std::visit([outBuffer, numSamples] (auto&& chosenModel) {
+            for (int i = 0; i < numSamples; ++i) {
+                float input[] = { outBuffer[i] };
+                outBuffer[i] += chosenModel.forward(input);
+            }
+        }, model);
     }
 }
 
 void NeuralProcessor::processBlock(juce::AudioBuffer<float>& buffer,
                                        juce::MidiBuffer& midiMessages) {
     processAbstractBlock<float>(buffer, midiMessages);
-}
-
-void NeuralProcessor::processBlock(juce::AudioBuffer<double>& buffer,
-                                       juce::MidiBuffer& midiMessages) {
-    processAbstractBlock<double>(buffer, midiMessages);
 }
 
 
@@ -179,16 +167,11 @@ juce::AudioProcessorEditor* NeuralProcessor::createEditor()
 //==============================================================================
 void NeuralProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
     juce::ignoreUnused (destData);
 }
 
 void NeuralProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
     juce::ignoreUnused (data, sizeInBytes);
 }
 
--- PluginProcessor.h
+++ PluginProcessor.h
@@ -1,13 +1,24 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <string>
+#include <memory>
 
+#include "RTNeural/RTNeural/RTNeural.h"
 //==============================================================================
 class NeuralProcessor : public juce::AudioProcessor
 {
 public:
+    using LightModel = RTNeural::ModelT<float, 1, 1, RTNeural::LSTMLayerT<float, 1, 6>, 
+            RTNeural::DenseT<float, 6, 1>>;
+    using MediumModel = RTNeural::ModelT<float, 1, 1, RTNeural::LSTMLayerT<float, 1, 16>,
+          RTNeural::DenseT<float, 16, 1>>;
+    using HeavyModel = RTNeural::ModelT<float, 1, 1, RTNeural::LSTMLayerT<float, 1, 32>,
+          RTNeural::DenseT<float, 32, 1>>;
+    using ModelVariant = std::variant<LightModel, MediumModel, HeavyModel>;
     //==============================================================================
     NeuralProcessor();
+    NeuralProcessor(juce::String jsonPath);
     ~NeuralProcessor() override;
     //==============================================================================
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
@@ -19,7 +30,6 @@ public:
     void processAbstractBlock(juce::AudioBuffer<T>&, juce::MidiBuffer&);
 
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
-    void processBlock (juce::AudioBuffer<double>&, juce::MidiBuffer&) override;
 
     //==============================================================================
     juce::AudioProcessorEditor* createEditor() override;
@@ -44,7 +54,14 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    //==============================================================================
+    friend class NeuralProcessorEditor;
+
+    bool loaded = false;
 private:
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NeuralProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NeuralProcessor)   
+    
+    ModelVariant model;
+    juce::AudioProcessorValueTreeState parameters;
 };
